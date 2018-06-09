644ef0bbae041b2a4ca2d1287c26a431eb59ed6e
Garrett Luecke <garrett_luecke@hotmail.com>
2018-06-08 21:10:33 -0500

    adds redis password

--- src/main/java/com/garasha/gedis/GedisAppConfig.java
+++ src/main/java/com/garasha/gedis/GedisAppConfig.java
@@ -3,6 +3,7 @@ package com.garasha.gedis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -22,6 +23,9 @@ public class GedisAppConfig {
         try {
             final URI uri = new URI(System.getenv("REDIS_URL"));
             final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(uri.getHost(), uri.getPort());
+            String s = uri.getUserInfo().split(":", 2)[1];
+            RedisPassword password = RedisPassword.of(s);
+            redisStandaloneConfiguration.setPassword(password);
         return new JedisConnectionFactory(redisStandaloneConfiguration);
         } catch (URISyntaxException e) {
             e.printStackTrace();
